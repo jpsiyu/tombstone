bb93ce5b61f638308de394a8b663ae842210ceda
jpsiyu <jpsiyu@163.com>
2018-06-04 14:54:32 +0800

    nothing work plugins

--- webpack.config.js
+++ webpack.config.js
@@ -11,28 +11,6 @@ module.exports = {
         path: path.resolve(__dirname, 'client/public/bundle'),
         filename: '[name].bundle.js',
     },
-    optimization: {
-      splitChunks: {
-        chunks: "async",
-        minSize: 30000,
-        minChunks: 1,
-        maxAsyncRequests: 5,
-        maxInitialRequests: 3,
-        automaticNameDelimiter: '~',
-        name: true,
-        cacheGroups: {
-            vendors: {
-                test: /[\\/]node_modules[\\/]/,
-                priority: -10
-            },
-            default: {
-                minChunks: 2,
-                priority: -20,
-                reuseExistingChunk: true
-            }
-        }
-      }
-    },
     mode:'production',
     module:{
       rules:[
@@ -53,9 +31,4 @@ module.exports = {
         }
       ]
     },
-    plugins:[
-      new webpack.DefinePlugin({
-        'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV)}
-      }),
-    ]
 }
